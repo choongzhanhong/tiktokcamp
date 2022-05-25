4fddad350a1f0f9ea1b6f0f7037f35af88991cd9
AMD\joenerio <joenerio@amd.com>
2022-05-25 21:20:28 +0800

    Add score tracker

--- src/helpers.js
+++ src/helpers.js
@@ -1,15 +1,16 @@
 export function checkWin(word, correctLetters, wrongLetters) {
-    let status = 'win';
-  
-    word.split('').forEach(letter => {
-      if(!correctLetters.includes(letter)){
-        status = '';
+    if (word !== '') {
+      let status = 'win';
+      word.split('').forEach(letter => {
+        if(!correctLetters.includes(letter)){
+          status = '';
+        }
+      });
+      
+      if(wrongLetters.length === 6) {
+          status = 'lose';
       }
-    });
-    
-    if(wrongLetters.length === 6) {
-        status = 'lose';
+      return status
     }
-  
-    return status
+    return ''  
   }
